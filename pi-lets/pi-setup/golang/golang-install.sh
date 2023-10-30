9f01fc3478037d8031d5b5a82744df8c28696059
JasolaCode <109664362+JasolaCode@users.noreply.github.com>
2023-10-30 21:11:16 +1100

    Update golang-install.sh

--- pi-lets/pi-setup/golang/golang-install.sh
+++ pi-lets/pi-setup/golang/golang-install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install vim
 mkdir ~/golang && cd ~/golang
 # version will change, alter the URL to match the new URL
@@ -21,24 +23,3 @@ echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 source ~/.bashrc
 # To check that it implemented fine
 echo $PATH
-# change the configuration in the file **/etc/dphys-swapfile **:
-sudo nano /etc/dphys-swapfile
-# The default value in Raspbian is:
-CONF_SWAPSIZE=100
-# We will need to change this to:
-CONF_SWAPSIZE=1024
-# Then you need to stop and start the service that manages the swapfile own Rasbian:
-sudo /etc/init.d/dphys-swapfile stop
-sudo /etc/init.d/dphys-swapfile start
-# You can then verify the amount of memory + swap by issuing the following command:
-free -m
-# Install the Bettercap dependencies
-sudo apt-get install build-essential libpcap-dev libusb-1.0-0-dev libnetfilter-queue-dev git
-# Next we'll get the bettercap repo into the /usr/local/bin/bettercap
-# go get github.com/bettercap/bettercap... This one didn't work
-git clone https://github.com/bettercap/bettercap.git
-cd bettercap
-# idk how the below line works... kinda neat!
-make build
-# idk how this one works either... also neat!
-make install
