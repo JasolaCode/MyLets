9355055ed990228bae14b615cad47e9a61337391
JasolaCode <joshsolano56@gmail.com>
2023-11-09 19:47:06 +1100

    changed smb-install for RaspB attempt

--- env-lets/smb-install.sh
+++ env-lets/smb-install.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-
+#
 #smb-install.sh
 
+set -e
+
 MYIP=$(ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet" | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1)
+read -p "Enter username of samba user: " SMBUSER
 
 sudo apt install samba -y
+echo "- Installed samba version: $(samba --version)"
+
 sudo tee -a /etc/samba/smb.conf <<EOF
-[jasola]
-	path = /home/jasola
+[${SMBUSER}]
+	path = /home/${SMBUSER}
 	browseable = yes
 	read only = no
 EOF
 
+echo "- Starting smbd/nmbd services "
 sudo service smbd start
 sudo service nmbd start
 
-echo "Create a user with: sudo smbpasswd -a jasola"
+echo "- Creating samba user $SMBUSER"
+sudo smbpasswd -a $SMBUSER
 echo "Use the UNC Path: \\$MYIP"
